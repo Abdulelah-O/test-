cbd36dbae3235116efd18f713e2c9281e3adbb4c
Abdulelah Bin Obaid <107353077+Abdulelah-O@users.noreply.github.com>
2025-08-06 03:07:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello Render"}
+
+if _name_ == "_main_":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    print("🚀 Running on port", port)
+    uvicorn.run("test_api:app", host="0.0.0.0", port=port)
