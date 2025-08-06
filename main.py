517a192bdbc22a355c35530fde9b890a11a05248
Abdulelah Bin Obaid <107353077+Abdulelah-O@users.noreply.github.com>
2025-08-06 03:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello Render"}
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     print("🚀 Running on port", port)
 
     uvicorn.run("test_api:app", host="0.0.0.0",port=port)
+
